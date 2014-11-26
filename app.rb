ddd3aeddf6adbb86dd43b474d2f9174f368f56b9
Cori Snedecor <coriannas@yahoo.com>
2014-11-26 18:13:06 -0500

    Added rudimentary search

--- app.rb
+++ app.rb
@@ -9,11 +9,6 @@ def contacts
   @contacts = Contact.all
 end
 
-def limited_contacts
-    @limit = 5
-    @contacts = Contact.limit(@limit).offset(@limit * @page - @limit)
-end
-
 def increment_page
   if @page > 1
     @back = @page-1
@@ -27,14 +22,25 @@ def increment_page
   end
 end
 
+def search_contacts
+  @limit = 5
+  if params[:search]
+    @contacts = Contact.where("first_name ILIKE ? OR last_name ILIKE ?",
+    params[:search], params[:search]).limit(@limit).offset(@limit*@page -@limit)
+  else
+    @contacts = Contact.limit(@limit).offset(@limit * @page - @limit)
+  end
+end
+
 get '/' do
+  @search = params[:search]
   if params[:page]
     @page = params[:page].to_i
   else
     @page = 1
   end
+  search_contacts
   increment_page
-  limited_contacts
   erb :index
 end
 
